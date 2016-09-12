1ee754a6bf4f1379a2e51907def644b0b5d18c18
Ilya Ryzhenkov <orangy@jetbrains.com>
2016-09-12 14:50:45 +0300

    Separate configuration and feature type in ApplicationFeature, unify structure of code

--- ktor-samples/ktor-samples-json/src/org/jetbrains/ktor/samples/json/JsonApplication.kt
+++ ktor-samples/ktor-samples-json/src/org/jetbrains/ktor/samples/json/JsonApplication.kt
@@ -8,7 +8,9 @@ import org.jetbrains.ktor.features.http.*
 import org.jetbrains.ktor.http.*
 import org.jetbrains.ktor.logging.*
 import org.jetbrains.ktor.request.*
+import org.jetbrains.ktor.response.*
 import org.jetbrains.ktor.routing.*
+import org.jetbrains.ktor.transform.*
 import org.jetbrains.ktor.util.*
 
 data class Model(val name: String, val items: List<Item>)
@@ -30,28 +32,32 @@ class JsonApplication : ApplicationFeature<Application, Unit> {
             install(DefaultHeaders)
             install(CallLogging)
 
-            intercept(ApplicationCallPipeline.Infrastructure) { call ->
-                if (call.request.accept() == "application/json") {
-                    call.response.pipeline.intercept(RespondPipeline.Before) {
-                        val message = subject.message
-                        when (message) {
-                            is Item, is Model -> {
-                                call.respond(TextContent(ContentType.Application.Json, GsonBuilder().create().toJson(message)))
-                            }
-                        }
+        val gson = GsonBuilder().create()
+        intercept(ApplicationCallPipeline.Infrastructure) { call ->
+            if (HeaderValue("application/json") in call.request.acceptItems()) {
+                call.transform.register<Any> { value ->
+                    val responseContentType = call.response.headers[HttpHeaders.ContentType]?.let { ContentType.parse(it) }
+                    when (responseContentType) {
+                        ContentType.Application.Json -> TextContent(ContentType.Application.Json, gson.toJson(value))
+                        else -> value
                     }
                 }
             }
+        }
 
-            val model = Model("root", listOf(Item("A", "Apache"), Item("B", "Bing")))
-            routing {
-                get("/v1") {
-                    call.respond(model)
-                }
-                get("/v1/item/{key}") {
-                    call.respond(model.items.first { it.key == call.parameters["key"] })
-                }
+        val model = Model("root", listOf(Item("A", "Apache"), Item("B", "Bing")))
+        routing {
+            get("/v1") {
+                call.respond(ContentType.Application.Json, model)
+            }
+            get("/v1/item/{key}") {
+                call.respond(ContentType.Application.Json, model.items.first { it.key == call.parameters["key"] })
             }
         }
     }
+}
+
+fun ApplicationCall.respond(contentType: ContentType, value: Any): Nothing {
+    response.contentType(contentType)
+    respond(value)
 }
