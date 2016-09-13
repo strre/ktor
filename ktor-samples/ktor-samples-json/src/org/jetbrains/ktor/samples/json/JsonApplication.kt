0936b334ef5c3b2914f4fc65ee8f04a3cc83602b
Ilya Ryzhenkov <orangy@jetbrains.com>
2016-09-13 18:29:54 +0300

    Fix compilation after merging ApplicationFeature additional type parameter and changes in Application definition.

--- ktor-samples/ktor-samples-json/src/org/jetbrains/ktor/samples/json/JsonApplication.kt
+++ ktor-samples/ktor-samples-json/src/org/jetbrains/ktor/samples/json/JsonApplication.kt
@@ -16,7 +16,7 @@ import org.jetbrains.ktor.util.*
 data class Model(val name: String, val items: List<Item>)
 data class Item(val key: String, val value: String)
 
-class JsonApplication : ApplicationFeature<Application, Unit> {
+class JsonApplication : ApplicationFeature<Application, Unit, Unit> {
     override val key = AttributeKey<Unit>(javaClass.simpleName)
 
     /*
@@ -32,26 +32,27 @@ class JsonApplication : ApplicationFeature<Application, Unit> {
             install(DefaultHeaders)
             install(CallLogging)
 
-        val gson = GsonBuilder().create()
-        intercept(ApplicationCallPipeline.Infrastructure) { call ->
-            if (HeaderValue("application/json") in call.request.acceptItems()) {
-                call.transform.register<Any> { value ->
-                    val responseContentType = call.response.headers[HttpHeaders.ContentType]?.let { ContentType.parse(it) }
-                    when (responseContentType) {
-                        ContentType.Application.Json -> TextContent(ContentType.Application.Json, gson.toJson(value))
-                        else -> value
+            val gson = GsonBuilder().create()
+            intercept(ApplicationCallPipeline.Infrastructure) { call ->
+                if (HeaderValue("application/json") in call.request.acceptItems()) {
+                    call.transform.register<Any> { value ->
+                        val responseContentType = call.response.headers[HttpHeaders.ContentType]?.let { ContentType.parse(it) }
+                        when (responseContentType) {
+                            ContentType.Application.Json -> TextContent(ContentType.Application.Json, gson.toJson(value))
+                            else -> value
+                        }
                     }
                 }
             }
-        }
 
-        val model = Model("root", listOf(Item("A", "Apache"), Item("B", "Bing")))
-        routing {
-            get("/v1") {
-                call.respond(ContentType.Application.Json, model)
-            }
-            get("/v1/item/{key}") {
-                call.respond(ContentType.Application.Json, model.items.first { it.key == call.parameters["key"] })
+            val model = Model("root", listOf(Item("A", "Apache"), Item("B", "Bing")))
+            routing {
+                get("/v1") {
+                    call.respond(ContentType.Application.Json, model)
+                }
+                get("/v1/item/{key}") {
+                    call.respond(ContentType.Application.Json, model.items.first { it.key == call.parameters["key"] })
+                }
             }
         }
     }
